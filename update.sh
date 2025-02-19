00654953f0bab3863c8da73554b6e053239fd8fc
maxocean <39007967+haohaoget@users.noreply.github.com>
2025-02-19 11:20:11 +0800

    Update update.sh
    Signed-off-by: maxocean <39007967+haohaoget@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -127,12 +127,13 @@ update_golang() {
 }
 
 install_small8() {
+    # quickstart luci-app-quickstart luci-app-istorex 
     ./scripts/feeds install -p small8 -f xray-core xray-plugin dns2tcp dns2socks haproxy hysteria \
         naiveproxy shadowsocks-rust sing-box v2ray-core v2ray-geodata v2ray-geoview v2ray-plugin \
         tuic-client chinadns-ng ipt2socks tcping trojan-plus simple-obfs shadowsocksr-libev \
         luci-app-passwall alist luci-app-alist smartdns luci-app-smartdns v2dat mosdns luci-app-mosdns \
         adguardhome luci-app-adguardhome ddns-go luci-app-ddns-go taskd luci-lib-xterm luci-lib-taskd \
-        luci-app-store luci-app-cloudflarespeedtest \  # quickstart luci-app-quickstart luci-app-istorex 
+        luci-app-store luci-app-cloudflarespeedtest \
         luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-homeproxy \
         luci-app-amlogic nikki luci-app-nikki tailscale
 }
